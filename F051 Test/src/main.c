a44621de4c7ad6240432055cf92f200322bbcf3b
nickvonklemp <nick.vonklemp@gmail.com>
2016-08-09 18:14:12 +0200

    Commit 002

--- F051 Test/src/main.c
+++ F051 Test/src/main.c
@@ -24,7 +24,8 @@
 //====================================================================
 // GLOBAL VARIABLES
 //====================================================================
-uint16_t adcval;
+uint16_t adcval=0,timeval=0;
+_Bool bit;
 char lcdstring[16];
 //====================================================================
 // FUNCTION DECLARATIONS
@@ -33,69 +34,80 @@ void init_ports(void);
 void init_adc(void);
 void init_nvic(void);
 void init_extio(void);
+void init_tim14(void);
+void TIM14_IRQHandler(void);
 //====================================================================
 // MAIN FUNCTION
 //====================================================================
 void main (void)
 {
-	adcval=0;
 	init_LCD();								// Initialise lcd
-	lcd_putstring("EEE3017W Prac 6");		// Display string on line 1
-	lcd_command(LINE_TWO);					// Move cursor to line 2
-	lcd_putstring("**names**");				// Display string on line 2
+	lcd_putstring("Testing");		// Display string on line 1
 	init_ports();
+	init_adc(); //Init ADC
+	init_extio();//Extio init
+	init_tim14();//init TIM14
 	while(GPIOA->IDR & GPIO_IDR_0){} //waits
-	init_adc();
-	init_extio();
 	init_nvic();
 	lcd_command(CLEAR);
 	for(;;){
+		adcval=ADC1->DR; //read from ADC
 		lcd_command(CURSOR_HOME);
-		lcd_putstring("Test");
-		GPIOB->ODR=adcval;
-		lcd_command(LINE_TWO);
-		sprintf(lcdstring,"%d",adcval);
+		sprintf(lcdstring,"ADC: %d",adcval);
 		lcd_putstring(lcdstring);
-	};								// Loop forever
-}											// End of main
+		lcd_command(LINE_TWO);
+		int temp = (RCC->CFGR);
+		sprintf(lcdstring,"RCC: %d",temp);
+		lcd_putstring(lcdstring);
+		TIM14->CCR1= 14692*(0.7+1.3*(adcval/255.0));
+	}
+}
+
 void init_ports(void){
-	RCC->AHBENR|=RCC_AHBENR_GPIOAEN|RCC_AHBENR_GPIOBEN;
+	RCC->AHBENR|=RCC_AHBENR_GPIOAEN|RCC_AHBENR_GPIOBEN;			//enable GPIOA & GPIOB Clocks
 	GPIOB->MODER|=(GPIO_MODER_MODER0_0|GPIO_MODER_MODER1_0|
 			GPIO_MODER_MODER2_0|GPIO_MODER_MODER3_0|
 			GPIO_MODER_MODER4_0|GPIO_MODER_MODER5_0|
-			GPIO_MODER_MODER6_0|GPIO_MODER_MODER7_0|
-			GPIO_MODER_MODER8_0|GPIO_MODER_MODER10_0|GPIO_MODER_MODER11_0);
-	GPIOA->MODER &= ~(GPIO_MODER_MODER0|GPIO_MODER_MODER1|GPIO_MODER_MODER2|GPIO_MODER_MODER3);
-	GPIOA->MODER|=GPIO_MODER_MODER5;
-	GPIOA->PUPDR|=(GPIO_PUPDR_PUPDR0_0|GPIO_PUPDR_PUPDR1_0|GPIO_PUPDR_PUPDR2_0|GPIO_PUPDR_PUPDR3_0);
-	GPIOB->ODR=(0xff|GPIO_ODR_11);
+			GPIO_MODER_MODER6_0|GPIO_MODER_MODER7_0); //set GPIOB0-7 as outputs
+	GPIOA->MODER &= ~(GPIO_MODER_MODER0|GPIO_MODER_MODER1|GPIO_MODER_MODER2|GPIO_MODER_MODER3); //PA0-3 Inputs
+	GPIOA->MODER|=GPIO_MODER_MODER5;//PA5 Analog mode input
+	GPIOA->MODER|=(GPIO_MODER_MODER7_1); //PA7 Alternate Function
+	GPIOA->AFR[0]|=(0b0100<<28); //PA7 AF4
+	GPIOA->PUPDR|=(GPIO_PUPDR_PUPDR0_0|GPIO_PUPDR_PUPDR1_0|GPIO_PUPDR_PUPDR2_0|GPIO_PUPDR_PUPDR3_0);//Pull ups for PA0-PA3
 }
-void init_adc(void){
-	RCC->APB2ENR|=RCC_APB2ENR_ADCEN;
-	ADC1->CHSELR|=ADC_CHSELR_CHSEL5;
-	ADC1->CFGR1|=(ADC_CFGR1_RES_1);
-	ADC1->IER|=ADC_IER_EOCIE;
-	ADC1->CR|=ADC_CR_ADEN;
-	while((ADC1->ISR&ADC_ISR_ADRDY)){}
-	ADC1->CR|=ADC_CR_ADSTART;
+
+void init_adc(void){ //Init ADC Continuous conversion, not interrupts
+	RCC->APB2ENR|=RCC_APB2ENR_ADCEN; //Clock ADC
+	ADC1->CHSELR|=ADC_CHSELR_CHSEL5; //Select CH5, PA5 -> ADC In5
+	ADC1->CFGR1|=(ADC_CFGR1_RES_1|ADC_CFGR1_CONT); //8Bit res & Continuous
+	ADC1->CR|=ADC_CR_ADEN; //Enable ADC
+	while((ADC1->ISR&ADC_ISR_ADRDY)){} //RDY Flag
+	ADC1->CR|=ADC_CR_ADSTART; //Starts continuous conversion
 }
 void init_nvic(void){
-	NVIC_EnableIRQ(ADC1_COMP_IRQn);
-	NVIC_EnableIRQ(EXTI0_1_IRQn);
+	NVIC_EnableIRQ(EXTI0_1_IRQn); //Enables IRQ for EXTI 0 & 1
 }
-void ADC1_COMP_IRQHandler(void){
-	ADC1->ISR&=~(ADC_ISR_EOC);
-	adcval=(ADC1->DR);
-}
-void init_extio(void){
-	RCC->APB2ENR|= RCC_APB2ENR_SYSCFGCOMPEN;
-	SYSCFG->EXTICR[1]|=SYSCFG_EXTICR1_EXTI0_PA;
-	EXTI->IMR|=EXTI_IMR_MR1;
-	EXTI->FTSR|=EXTI_FTSR_TR1;
+void init_extio(void){	//Init EXTI-0
+	RCC->APB2ENR|= RCC_APB2ENR_SYSCFGCOMPEN; //Clocks cfg comp
+	SYSCFG->EXTICR[1]|=SYSCFG_EXTICR1_EXTI1_PA; //Sets Control Register1 for EXTI1 PA1
+	EXTI->IMR|=EXTI_IMR_MR1; //Unmasks input
+	EXTI->FTSR|=EXTI_FTSR_TR1; //sets for falling edge
 }
 void EXTI0_1_IRQHandler(void){
 	EXTI->PR |= EXTI_PR_PR1;
-	ADC1->CR|=ADC_CR_ADSTART;
+	//SW1 IRQ Handler
+}
+void init_tim14(void){
+	RCC->APB1ENR|= RCC_APB1ENR_TIM14EN;
+	TIM14->PSC=3;
+	TIM14->ARR=48485;
+	TIM14->CCMR1|=(TIM_CCMR1_OC1M_2|TIM_CCMR1_OC1M_1);
+	TIM14->CCR1=14692*(0.7+1.3*(127.5/255.0));
+	TIM14->CCER|= (TIM_CCER_CC1E);
+	TIM14->CR1|=TIM_CR1_CEN;
+}
+void TIM14_IRQHandler(void){
+	TIM14->SR&=~(TIM_SR_UIF);
 }
 //********************************************************************
 // END OF PROGRAM
